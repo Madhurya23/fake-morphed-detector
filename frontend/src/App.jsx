6e55ea701234b77f817ea7c16197d293e9120ecb
Diya Ghosh <diyaslg2004@gmail.com>
2026-02-15 15:55:51 +0530

    Final frontend fixes and cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import AppRoutes from "./routes/AppRoutes";
-
-export default function App() {
-  return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <Navbar />
-
-      <main className="flex-grow">
-        <AppRoutes />
-      </main>
-
-      <Footer />
-    </div>
-  );
-}
+import React from "react"; 
+import Navbar from "./components/Navbar"; 
+import Footer from "./components/Footer"; 
+import AppRoutes from "./routes/AppRoutes"; 
+export default function App()
+{ 
+  return 
+  ( 
+  <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100"> 
+  <Navbar /> 
+  <main className="flex-grow"> 
+    <AppRoutes /> 
+    </main> 
+    <Footer /> 
+    </div> 
+    ); 
+  }
